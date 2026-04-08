4354d88d453f0d02c7e404b309ff1cc758ac3052
ALFREDO MARTIN CASTAÑEDA HERNANDEZ <alfredo.castaneda@unison.mx>
2026-04-08 11:12:40 -0700

    Restore default trigger handling in batch runner

--- run_all_scenarios.sh
+++ run_all_scenarios.sh
@@ -33,7 +33,7 @@ MC_INPUT='/eos/user/c/castaned/Cosmics/LooseMuCosmic_Bin-P-10to3000-T0-Minus50to
 DATA_INPUT='/eos/user/c/castaned/Cosmics/Cosmics/CosmicsPPreco-CosmicDTLocalReco_Run2024I_Ntuples_v3/260310_205924/0000/*.root'
 
 REL_PT_ERR_CUT="0.2"
-TRIGGER_ARGS="--require-trigger-for-mc"
+TRIGGER_ARGS=""
 
 # =========================================
 # UTILS
